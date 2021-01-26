9e04eef655f7d724c9c8aa1c75df082585619e68
Peter <pgkutc@gmail.com>
2021-01-25 20:57:01 -0500

    init

---
+++ src/test/java/com/kutchen/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kutchen.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
